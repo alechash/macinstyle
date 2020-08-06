a8b15d9504ff527eaf26a5ca1f3ed44825eff925
Jude Wilson <judewilson2024@gmail.com>
2020-08-06 16:23:39 -0400

    modals and buttons

---
+++ src/macinstyle.js
@@ -0,0 +1,9 @@
+/*
+
+Copyright Jude Wilson 2020
+Macinstyle is owned published and controlled by Jude Wilson
+mrwinson@pm.me
+github repo: https: //github.com/mr-winson/macinstyle
+github account: https: //github.com/mr-winson
+
+*/
